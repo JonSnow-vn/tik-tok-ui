4c99e6f3c0b72d743ddec59efcbec945de598b54
JonSnow-vn <toiselamduoc0110@gmail.com>
2022-05-23 21:55:29 +0700

    update logo

---
+++ src/assets/image/index.js
@@ -0,0 +1,16 @@
+const logo = {
+    path: require("~/assets/image/logo.svg").default
+};
+
+const deleteIcon = {
+    path: require("~/assets/image/delete.svg").default
+};
+
+const loadingIcon = {
+    path: require("~/assets/image/loading.svg").default
+};
+
+const searchIcon = {
+    path: require("~/assets/image/search.svg").default
+};
+export {logo, deleteIcon, loadingIcon, searchIcon};
--- src/components/Layout/components/Header/index.js
+++ src/components/Layout/components/Header/index.js
@@ -1,14 +1,31 @@
 import styles from './Header.module.scss'
 import classNames from 'classnames/bind';
+import {logo, deleteIcon, loadingIcon, searchIcon} from '~/assets/image';
 
 const cx = classNames.bind(styles)
-
 
 function Header() {
     return (
         <header className={cx('wrapper')}>
             <div className={cx('inner')}>
-                <h2>Header</h2> 
+                <div className={cx('logo')}>
+                    <img src={logo.path} alt="InconTiktok" />
+                </div>
+                <div className={cx('search')}>
+                    <input placeholder='Tìm kiếm tài khoản và video' spellCheck={false} />
+                    <div className={cx('delete-icon')}>
+                        <img src={deleteIcon.path} alt="delete-icon" />
+                    </div>
+                    {/* <div className={cx('loading-icon')}>
+                        <img src={loadingIcon.path} alt="loading-icon" />
+                    </div> */}
+                    <button className={cx('search-icon')}>
+                        <img src={searchIcon.path} alt="search-icon" />
+                    </button>
+                </div>
+                <div className={cx('action')}>
+
+                </div>
             </div>
         </header>
          
